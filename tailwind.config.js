0fccadb47dc7064ba0583413da2ce145596709d0
Michael <michael.cacciano@gmail.com>
2020-06-25 22:30:04 -0400

    fin

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+    fontFamily: {
+      rubik: ['Rubik', 'Helvetica', 'Arial', 'sans-serif'],
+      roboto: ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+    },
+  },
+  variants: {},
+  plugins: [],
+};
