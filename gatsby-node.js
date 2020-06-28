ffe0829f47f0f174a1683cd5b565650f056b0e78
Michael <michael.cacciano@gmail.com>
2020-06-27 20:37:06 -0400

    part-4

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,59 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+
+  if (node.internal.type === 'MarkdownRemark') {
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: `${__dirname}/content/blog`,
+    });
+
+    console.log('slug', slug);
+
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug,
+    });
+  }
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  const BlogPostTemplate = path.resolve('./src/templates/BlogPost.js');
+
+  const {
+    data: { posts },
+  } = await graphql(`
+    query BlogPostsQuery {
+      posts: allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              description
+              date
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  posts.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: BlogPostTemplate,
+      context: {
+        slug: node.fields.slug,
+      },
+    });
+  });
+};
--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -5,16 +5,16 @@ import PostsListItem from './PostsListItem';
 
 const PostsList = ({ posts }) => {
   return (
-    <ul>
-      {posts.edges.map(({ node }) => (
-        <PostsListItem frontmatter={node.frontmatter} />
+    <ul className="grid grid-cols-1 gap-10 w-full max-w-screen-xl mx-auto px-8 mt-8">
+      {posts.edges.map(({ node }, index) => (
+        <PostsListItem key={index} post={node} />
       ))}
     </ul>
   );
 };
 
 PostsList.propTypes = {
-  posts: PropTypes.array.isRequired,
+  posts: PropTypes.object.isRequired,
 };
 
 export default PostsList;
