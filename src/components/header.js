5a8e9e302e1afd119a821f7a7bbb26715d61a614
Michael <michael.cacciano@gmail.com>
2020-06-27 21:41:27 -0400

    committing code to github so we can easily host our blog on netlify

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,8 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+// this is a change to show git things
+
 const Header = ({ siteTitle }) => (
   <header className="h-16 bg-blue-600 text-white sticky top-0 z-50">
     <div className="flex justify-between items-center px-8 mx-auto w-full h-full max-w-screen-xl">
